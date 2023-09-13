339ce99842fb77e4065623a7db92375119f34104
The Special Character <hr@thespecialcharacter.com>
2023-09-13 19:39:57 +0530

    commit code

---
+++ basic.js
@@ -0,0 +1,15 @@
+
+
+const user = {
+    firstName: "Yagnesh",
+    lastName: "Modh",
+    role: "Full Stack Developer"
+}
+
+
+const fn = (data) => "Hi, I am " + data.firstName;
+
+
+
+console.log(fn(user)); 
+// Hi, I am yagnesh Modh, and I am full stack developer
