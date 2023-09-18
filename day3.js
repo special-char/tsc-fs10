0a8caad140a022214eb5e88491abfcd2152b4610
Jayni Patel <maggiepatel123@gmail.com>
2023-09-18 19:33:41 +0530

    js commit

---
+++ day3.js
@@ -0,0 +1,10 @@
+
+
+//male >> profession cricketer
+//female >> profession actor
+
+//female << remove age from obj
+//male << update profession to developer
+
+//read dynamic data of firstname and lastname
+//create fullName
