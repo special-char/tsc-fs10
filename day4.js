99ccd16a9dec4ba61fa2ccac974cd9ca209200b9
Jayni Patel <maggiepatel123@gmail.com>
2023-09-26 16:20:53 +0530

    third commit

---
+++ day4.js
@@ -0,0 +1,79 @@
+//sum of elements in array
+// const arr=[1,2,4,8,9,10,13,15]
+
+// let sum=0;
+// for (let i = 0;i < arr.length; i++)
+// {
+// const element=arr[i];
+// sum=sum+element;
+// console.log(sum);
+// }
+// console.log(sum)
+
+//descending order
+// const array = [3, 5, 1, 8, 9, 14, 18, 2, 4];
+// function getMaxValue(data, index) {
+//   for (let i = 0; i < data.length; i++) {
+//     console.log(array[i]);
+//     for (let j = i + 1; j < data.length; j++) {
+//       if (array[i] < array[j]) {
+//         const first = array[j];
+//         array[j] = array[i];
+//         array[i] = first;
+//       }
+//     }
+//   }
+// }
+// getMaxValue(array);
+// console.log(array);
+
+//descending order and dynamically fetch any highest value
+// const array = [3, 5, 1, 8, 9, 14, 18, 2, 4];
+// function getMaxValue(data, index) {
+//   for (let i = 0; i < data.length; i++) {
+//     console.log(array[i]);
+//     for (let j = i + 1; j < data.length; j++) {
+//       if (array[i] < array[j]) {
+//         const first = array[j];
+//         array[j] = array[i];
+//         array[i] = first;
+//       }
+//     }
+//   }
+// }
+// getMaxValue(array);
+// console.log(array);
+// console.log(array[1 - 1]);
+
+// ascending order
+// const arrayasc = [3, 5, 1, 8, 9, 14, 18, 2, 4];
+// function getMaxValue(data, index) {
+//   for (let i = 0; i < data.length; i++) {
+//     console.log(arrayasc[i]);
+//     for (let j = i + 1; j < data.length; j++) {
+//       if (arrayasc[i] > arrayasc[j]) {
+//         const first = arrayasc[j];
+//         arrayasc[j] = arrayasc[i];
+//         arrayasc[i] = first;
+//       }
+//     }
+//   }
+// }
+// getMaxValue(arrayasc);
+// console.log(arrayasc);
+
+
+
+
+
+//Value equal to index value 145
+
+//Print alternate elements of an array 146
+
+//Palindromic Array 147
+
+//Swap kth elements 148 150
+
+//to do
+//Display longest name
+//Perfect Arrays
